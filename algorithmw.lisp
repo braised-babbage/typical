4abbe404995090f59eb791d41a12a1330b11f4ec
erik <erik@cadlag.org>
2018-09-03 11:49:06 -0700

    initial implementation of hm inference

---
+++ algorithmw.lisp
@@ -0,0 +1,129 @@
+;;; Hindley-Milner style type inference.
+;;;
+;;; At a high level, the problem we want to solve is something like
+;;; this: given a term in the untyped lambda calculus, can we recover
+;;; the "most general" type (aka 'principal type scheme') for the
+;;; term? The code below implements the famous 'Algorithm W', which
+;;; solves this problem in the slightly more general case allowing for
+;;; let-polymorphism. It is based on a Scala implementation
+;;; presented by Odersky in Chapter 16 of Scala by Example. For more
+;;; information, see the bibliography at the end.
+
+(declaim (optimize (debug 3)))
+
+;;; Types
+
+;;; A type is one of the following:
+;;;    - a type variable (a non-null symbol)
+;;;    - an arrow (-> t1 t2) where t1,t2 are types
+;;;    - a type constructor (foo t1 ...)
+
+
+(defun free-variables (type)
+  "Return a list of free variables in the given type."
+  (if (symbolp type)
+      (list type)
+      ;; either -> or a type constructor
+      (remove-duplicates (mappend #'free-variables (rest type)))))
+
+;;; A substitution is an alist mapping type variables to types.
+
+(defun substitute-types (subst type)
+  "Replace type variables with their values in the given type."
+  (if (symbolp type)			; type variable
+      (let ((val (lookup subst type)))
+	   (if val
+	       (substitute-types subst val)
+	       type))
+      ;; either -> or a type constructor
+      (cons (first type)	       
+	    (mapcar #'(lambda (type) (substitute-types subst type))
+		    (rest type)))))
+
+;;; The type inference algorithm requires that we be able to unify types.
+
+(defun unify (u v &optional (subst nil))
+  "Compute a substitution making two types equal, if possible."
+  (let ((su (substitute-types subst u))
+	(sv (substitute-types subst v)))
+    (cond ((or (null su) (null sv))
+	   (error "cannot unify ~a and ~a" u v))
+	  ((and (symbolp su) (symbolp sv) (eq su sv))
+	   subst)
+	  ((and (symbolp su) (not (member su (free-variables sv))))
+	   (extend subst su sv))
+	  ((symbolp sv)
+	   (unify v u subst))
+	  ((eq (first su) (first sv))
+	   (unify-all (rest su) (rest sv) subst))
+	  (t  (error "cannot unify ~a and ~a" u v)))))
+
+(defun unify-all (ts us subst)
+  "Unify two lists of types, successively extending an initial
+substitution."
+  (cond ((and (null ts) (null us))
+	 subst)
+	((or (null ts) (null us))
+	 (error "cannot unify sequences of mismatched length"))
+	(t (let ((subst2 (unify (first ts) (first us) subst)))
+	     (unify-all (rest ts) (rest us) subst2)))))
+
+
+
+;;; Type Schemes are basically types with some quantified type variables.
+;;; For example, \forall a (-> a a).
+
+(defstruct type-scheme
+  "A type scheme consists of a type and some universally quantified type variables."
+  vars type)
+
+(defun new-instance (scheme)
+  "Replace the bound variables of a type scheme with a fresh set."
+  (let* ((vars (type-scheme-vars scheme))
+	 (fresh (mapcar #'make-type-variable vars))
+	 (subst (extend nil vars fresh)))
+    (substitute-types subst (type-scheme-type scheme))))
+
+(defvar *variable-count* 0)
+
+(defun make-type-variable (&optional (prefix 't))
+  "Make a fresh type variable with the given prefix."
+  (intern (format nil "~a~d" prefix (incf *variable-count*))))
+
+
+(setq *default-type-context* nil)
+;;; Everything is curried!
+(define-type 'true '(boolean))
+(define-type 'false '(boolean))
+(define-type 'if    '(-> (boolean) (-> a (-> a a))))
+(define-type 'zero  '(integer))
+(define-type 'succ  '(-> (integer) (integer)))
+(define-type 'empty '(list a))
+(define-type 'cons  '(-> a (-> (list a) (list a))))
+(define-type 'isEmpty '(-> (list a) (boolean)))
+(define-type 'head  '(-> (list a) a))
+(define-type 'tail '(-> (list a) (list a)))
+(define-type 'fix '(-> a (-> a a)))
+
+
+;;; Bibliography
+;;;
+
+;;; Clément, D., Despeyroux, T., Kahn, G., & Despeyroux, J. (1986, August). A simple applicative language: Mini-ML.
+;;;      In Proceedings of the 1986 ACM conference on LISP and functional programming (pp. 13-27). ACM
+
+;;; Damas, L., & Milner, R. (1982, January). Principal type-schemes for functional programs.
+;;;      In Proceedings of the 9th ACM SIGPLAN-SIGACT symposium on Principles of programming languages (pp. 207-212). ACM
+
+;;; Garnock-Jones, T. (2012). Type Inference for ML.
+;;;      http://www.ccs.neu.edu/home/amal/course/7480-s12/inference-notes.pdf
+
+;;; Odersky, M. (2011). Scala by example.
+;;;      http://www.scala-lang.org/docu/files/ScalaByExample.pdf
+
+;;; Pottier, F., & Rémy, D. (2005). The essence of ML type inference.
+;;;      http://gallium.inria.fr/~fpottier/publis/emlti-final.pdf
+
+;;; Wand, M. (1987). A simple algorithm and proof for type inference.
+;;;      Fundamenta Informaticae, 10(2), 115-121.
+
